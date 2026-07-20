b34e7eb102315e6a14d0bd7182dcbd40d5e7e4ac
Yakkobi <jacobwacevedo@gmail.com>
2026-07-19 22:54:18 -0400

    Add word lists
    Added a word list of well known animals, colors,
    and adjectives that can be used to generate a
    password from choosing between the 3.

---
+++ script.js
@@ -0,0 +1,55 @@
+// prettier-ignore
+const animals = [
+  "Cat", "Dog", "Fox", "Owl", "Bee", "Ant", "Cow", "Pig", "Rat", "Bat",
+  "Cod", "Hen", "Ram", "Elk", "Doe", "Eel",
+  "Lion", "Wolf", "Bear", "Deer", "Duck", "Frog", "Goat", "Crab", "Fish",
+  "Bird", "Hawk", "Mule", "Newt", "Seal", "Swan", "Lamb", "Mole", "Toad",
+  "Puma", "Lynx", "Boar",
+  "Tiger", "Zebra", "Horse", "Sheep", "Panda", "Snake", "Whale", "Shark",
+  "Eagle", "Otter", "Mouse", "Camel", "Moose", "Skunk", "Raven", "Robin",
+  "Rhino", "Gecko", "Llama", "Heron", "Crane", "Stork", "Goose", "Hyena",
+  "Monkey", "Rabbit", "Turtle", "Beaver", "Coyote", "Donkey", "Falcon",
+  "Jaguar", "Weasel", "Badger", "Salmon", "Gopher", "Possum", "Cougar",
+  "Bobcat", "Parrot", "Puffin", "Walrus", "Iguana", "Impala", "Marmot",
+  "Ferret", "Toucan", "Osprey", "Magpie", "Condor", "Jackal",
+  "Cheetah", "Buffalo", "Dolphin", "Leopard", "Giraffe", "Penguin",
+  "Mustang", "Peacock", "Ostrich", "Panther", "Wallaby", "Pelican",
+  "Sparrow", "Wildcat", "Warthog", "Buzzard", "Bulldog", "Catfish",
+  "Antelope", "Squirrel", "Kangaroo", "Elephant", "Chipmunk", "Flamingo",
+  "Hedgehog", "Cockatoo", "Mongoose", "Porpoise", "Reindeer", "Aardvark",
+  "Honeybee"
+];
+
+// prettier-ignore
+const colors = [
+  "Red", "Tan", "Jet",
+  "Blue", "Pink", "Gold", "Gray", "Grey", "Teal", "Navy", "Cyan", "Rose",
+  "Rust", "Plum", "Jade", "Ruby", "Aqua",
+  "Green", "Black", "White", "Brown", "Coral", "Amber", "Ivory", "Beige",
+  "Khaki", "Olive", "Slate", "Peach", "Lilac", "Mauve",
+  "Purple", "Orange", "Maroon", "Violet", "Indigo", "Silver", "Bronze",
+  "Copper", "Salmon", "Cherry", "Golden", "Yellow", "Sienna", "Auburn",
+  "Cobalt",
+  "Crimson", "Scarlet", "Emerald", "Magenta", "Fuchsia", "Apricot",
+  "Mustard", "Saffron",
+  "Burgundy", "Charcoal", "Lavender", "Mahogany"
+];
+
+// prettier-ignore
+const adjectives = [
+  "Big", "Sly", "Shy", "Wet", "Dry", "Hot", "Icy",
+  "Bold", "Calm", "Cool", "Dark", "Fast", "Kind", "Loud", "Mild", "Neat",
+  "Pure", "Rare", "Slow", "Soft", "Tall", "Warm", "Wild", "Wise", "Fair",
+  "Firm", "Keen", "Lean", "Sure", "Tidy", "Vast",
+  "Brave", "Fresh", "Happy", "Heavy", "Light", "Quiet", "Quick", "Sharp",
+  "Sweet", "Tough", "Young", "Eager", "Loyal", "Proud", "Solid", "Sunny",
+  "Witty", "Zesty", "Alert", "Rapid", "Merry", "Noble", "Hardy", "Brisk",
+  "Vivid",
+  "Clever", "Gentle", "Bright", "Sturdy", "Steady", "Mighty", "Cheery",
+  "Breezy", "Chilly", "Simple", "Honest", "Humble", "Active", "Frozen",
+  "Modern",
+  "Radiant", "Curious", "Elegant", "Playful", "Genuine", "Serious",
+  "Distant", "Amazing", "Awesome", "Careful", "Massive", "Restful",
+  "Fearless", "Peaceful", "Cheerful", "Powerful", "Faithful", "Youthful",
+  "Talented", "Colorful", "Adorable"
+];
